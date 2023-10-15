d9c1cc765cc274f2e4bab5a9adace2359b50c3a0
Lorena Ojeda <blanca.ojeda@udgvirtual.udg.mx>
2023-10-15 00:02:29 -0600

    3.6. El movimiento de una bala
    Se agregaron animaciones de estado

--- Assets/Scripts/cosmicManMovement.cs
+++ Assets/Scripts/cosmicManMovement.cs
@@ -11,6 +11,7 @@ public class cosmicManMovement : MonoBehaviour
     private float Horizontal;
     private bool Grounded;
     private float LastShoot;
+    public Animator animator;
 
     void Start()
     {
@@ -21,12 +22,15 @@ public class cosmicManMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Horizontal = Input.GetAxisRaw("Horizontal");
+        Horizontal = Input.GetAxisRaw("Horizontal") * Speed; 
 
         if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
-        Debug.DrawRay(transform.position, Vector3.down* 0.1f, Color.red);
+        animator.SetFloat("Speed", Mathf.Abs(Horizontal));
+
+
+        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
 
         if(Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
         {
@@ -57,6 +61,8 @@ public class cosmicManMovement : MonoBehaviour
         Vector3 direction;
         if(transform.localScale.x == 1.0f) direction = Vector3.right;
         else direction = Vector3.left;
+
+        animator.SetBool("isShooting", true);
 
         GameObject bullet = Instantiate(BulletPrefab, transform.position + direction * 0.1f, Quaternion.identity);
         bullet.GetComponent<BulletScript>().SetDirection(direction);
